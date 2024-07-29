8d0ea22f9be49f0642533227d6d6af16282bb99f
ibtasam116 <ibtasam116@gmail.com>
2024-07-29 11:45:58 +0500

    Employee

---
+++ app/employee/page.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <h1 className="text-4xl font-bold">
+          Next.js 14 CRUD and Search with Prisma Mysql | TailwindCSS DaisyUI
+        </h1>
+      </div>
+      <div className="overflow-x-auto">
+        <div className="mb-2 w-full text-right">
+          <Link href="/employee/create" className="btn btn-primary">
+            Create
+          </Link>
+        </div>
+        {/* <Search />
+        <Suspense key={query} fallback={<Spinner />}>
+          <TableData query={query} />
+        </Suspense> */}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
